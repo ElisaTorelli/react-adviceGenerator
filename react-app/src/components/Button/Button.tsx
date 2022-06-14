e72bffefb5c8dd02a5ca2a5211814bbcc617f891
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-14 20:30:43 +0200

    add code

---
+++ react-app/src/components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { click } from '@testing-library/user-event/dist/click';
+import React from 'react';
+import styles from "./button.module.css";
+import buttonImg from "../../img/icon-dice.svg";
+
+interface IButton{
+    clickValue: number,
+    handleClick: (params: number) => void
+}
+
+
+const Button: React.FC<IButton> = ({clickValue, handleClick}) => {
+
+    const handleClickValue = () => {
+        handleClick(clickValue + 1)
+    }
+
+    return (
+    <div>
+        <button onClick={handleClickValue} className={styles.btn}>
+            <img src={buttonImg} title='img' />
+        </button>
+    </div>
+    )
+}
+
+export default Button
---
+++ react-app/src/components/Card/Card.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import styles from "./card.module.css";
+import {IAdvice} from "../../App";
+import divider from "../../img/pattern-divider-desktop.svg"
+
+
+interface ICard {
+    advice: IAdvice
+}
+
+
+const Card: React.FC<ICard> = ({advice}) => {
+
+    return (
+        <div className={styles.advice__container}>
+            <h2 className={styles.advice}>ADVICE #{advice.id}</h2>
+            <h2 className={styles.text}>"{advice.text}"</h2>
+            <img src={divider} alt="dividerImg" className={styles.cardImg}/>
+        </div>
+    )
+}
+
+export default Card
